161e11ec8e8f2d15422bb433b4f10a5cfd671453
SSenitha <166741735+SSenitha@users.noreply.github.com>
2026-02-23 22:07:58 +0530

    Added second click coordinate input to popup.html Modified click function to accomodate second click

--- popup.js
+++ popup.js
@@ -1,41 +1,63 @@
 let clicking = false;
 
+// Func listner: Start Action
 document.getElementById("start").addEventListener("click", async () => {
+  // Get input1 values
   const x = parseInt(document.getElementById("x").value);
   const y = parseInt(document.getElementById("y").value);
   const interval = parseInt(document.getElementById("interval").value);
 
+  // Get input2 values
+  const x2 = parseInt(document.getElementById("x2").value);
+  const y2 = parseInt(document.getElementById("y2").value);
+  const interval2 = parseInt(document.getElementById("interval2").value);
+
   clicking = true;
 
+  // Get the active tab
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  // Scripting API : Start clicking the active tab
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: startClicking,
-    args: [x, y, interval]
+    args: [x, y, interval, x2, y2, interval2]
   });
 });
 
+
+// Func listner: Stop Action
 document.getElementById("stop").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  // Scripting API : Stop clicking the active tab
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: stopClicking
   });
 });
 
-function startClicking(x, y, interval) {
+// Button listner: Start Button
+function startClicking(x, y, interval, x2, y2, interval2) {
   if (window.autoClickInterval) return;
 
-  window.autoClickInterval = setInterval(() => {
+  window.autoClickInterval = setInterval(async () => {
     const el = document.elementFromPoint(x, y);
     if (el) {
       el.click();
     }
-  }, interval);
+
+    await new Promise(resolve => setTimeout(resolve, interval)); // Wait for the first click interval
+
+    const el2 = document.elementFromPoint(x2, y2);
+    if (el2) {
+      el2.click();
+    }
+
+  }, interval2);
 }
 
+// Button listner: Stop Button
 function stopClicking() {
   clearInterval(window.autoClickInterval);
   window.autoClickInterval = null;
