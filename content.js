d19c2e283a9641b23bd82ac227a4254f8eb8b4c3
SSenitha <166741735+SSenitha@users.noreply.github.com>
2026-03-12 00:37:00 +0530

    Version 1.0.0 finalizing -> updated to read from span instead of input -> removed test/Junk code

--- content.js
+++ content.js
@@ -9,18 +9,6 @@ document.addEventListener("mousemove", e => {
   mouseY = e.clientY;
 });
 
-// // listen for keypresses anywhere
-// document.addEventListener("keydown", e => {
-//   // send message to the extension popup (or background)
-
-//   console.log("Key pressed: " + e.key);
-//   chrome.runtime.sendMessage({
-//     key: e.key,
-//     x: mouseX,
-//     y: mouseY
-//   });
-// });
-
 // Listen for messages from popup.js
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "getMousePosition") {
